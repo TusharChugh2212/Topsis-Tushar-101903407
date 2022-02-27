543a56b1976a0435865e0ccb3cc963dec26bb929
TusharChugh2212 <67000091+TusharChugh2212@users.noreply.github.com>
2022-02-27 20:41:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Tushar Chugh',                   # Type in your name
   author_email = 'tchugh_be19@thapar.edu',      # Type in your E-Mail
   url = 'https://github.com/user/Topsis-Tushar-101903407',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/TusharChugh2212/Topsis-Tushar-101903407/archive/refs/tags/v_02.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[
           'numpy',
