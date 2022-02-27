2305ea2153d0ff32d52e05a2e4cbcd0cb61a2720
TusharChugh2212 <67000091+TusharChugh2212@users.noreply.github.com>
2022-02-27 20:18:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[
           'numpy',
-          'scrapeasy',
+          'pandas',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
